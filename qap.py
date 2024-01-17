134867969975d326a0bc85daf20f4ca981498924
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2024-01-17 13:12:12 +0800

    Refectored qap.py

--- qap.py
+++ qap.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 import util
+import time
 Q = util.genPrime(16)
+class Timer:
+    def __init__(self, text):
+        self.text = text
+    def __enter__(self):
+        print(self.text, end = ' ')
+        self.beg = time.time()
+    def __exit__(self, *args):
+        self.end = time.time()
+        print('{:.3f} sec'.format(self.end - self.beg))
 class Program:
     def __init__(self):
         self.gates = []
         self.vars = []
         self.asserts = []
+        self.reveals = []
     def gate_count(self):
         return len(self.gates)
     def var_count(self):
@@ -17,116 +28,117 @@ class Program:
         B = [[0 for _ in range(var_count)] for _ in range(gate_count)]
         C = [[0 for _ in range(var_count)] for _ in range(gate_count)]
         for i, gate in enumerate(self.gates):
-            xs, ys, zs = gate
-            for x, m in xs:
-                A[i][x] += m
-            for y, n in ys:
-                B[i][y] += n
-            for z, o in zs:
-                C[i][z] += o
+            x, y, z = gate
+            for k, v in x:
+                A[i][k] += v
+            for k, v in y:
+                B[i][k] += v
+            for k, v in z:
+                C[i][k] += v
         return A, B, C
     def witness(self, **kwargs):
         var_count = self.var_count()
         witness = [0 for _ in range(var_count)]
+        get = lambda x: sum(witness[k] * v for k, v in x) % Q
         for i, func in enumerate(self.vars):
-            witness[i] = func(witness, **kwargs)
+            witness[i] = func(get, **kwargs)
         for func in self.asserts:
-            assert func(witness, **kwargs)
+            assert func(get, **kwargs)
         return witness
-    def __bind(self, func): # create a new variable bound to a function, return its index
+    def __bind(self, func, reveal = False): # create a new variable bound to a function, return its index
         i = len(self.vars)
         self.vars.append(func)
+        if reveal:
+            self.reveals.append(i)
         return i
-    def VAR(self, name): # return a variable
-        o = self.__bind(lambda witness, **kwargs: kwargs[name])
-        return o
-    def VMUL(self, xs, ys, zs = [], N = 1): # return (xs * ys - zs) / N (mod Q)
-        o = self.__bind(lambda witness, **kwargs: (sum(witness[x] * m for x, m in xs) * pow(sum(witness[y] * n for y, n in ys), +1, Q) - sum(witness[z] * o for z, o in zs)) * pow(N, -1, Q) % Q)
-        self.gates.append((xs, ys, zs + [(o, N)]))
-        return o
-    def VDIV(self, zs, ys, xs = [], N = 1): # return (xs / ys - zs) / N (mod Q)
-        o = self.__bind(lambda witness, **kwargs: (sum(witness[z] * o for z, o in zs) * pow(sum(witness[y] * n for y, n in ys), -1, Q) - sum(witness[x] * m for x, m in xs)) * pow(N, -1, Q) % Q)
-        self.gates.append((xs + [(o, N)], ys, zs))
-        return o
-    def MUL(self, a, b): # return a * b (mod Q)
-        return self.VMUL([(a, 1)], [(b, 1)])
-    def DIV(self, c, b): # return c / b (mod Q)
-        return self.VDIV([(c, 1)], [(b, 1)])
-    def POW(self, e, a, N): # return a ** N (mod Q)
+    def VAR(self, name, reveal = False): # return a variable
+        return [(self.__bind(lambda get, **kwargs: kwargs[name], reveal), 1)]
+    def RET(self, al): # reveal the value of a variable
+        cl = [(self.__bind(lambda get, **kwargs: get(al), True), 1)]
+        self.gates.append(([], [], self.SUB(cl, al)))
+        return cl
+    def MULN(self, al, N): # return a * N (mod Q)
+        return [(i, m * N % Q) for i, m in al]
+    def DIVN(self, al, N): # return a / N (mod Q)
+        return [(i, m * util.modinv(N, Q) % Q) for i, m in al]
+    def ADD(self, al, bl): # return a + b (mod Q)
+        return al + bl
+    def SUB(self, al, bl): # return a - b (mod Q)
+        return al + [(i, -m % Q) for i, m in bl]
+    def MUL(self, al, bl): # return a * b (mod Q)
+        cl = [(self.__bind(lambda get, **kwargs: get(al) * get(bl) % Q), 1)]
+        self.gates.append((al, bl, cl))
+        return cl
+    def DIV(self, cl, bl): # return c / b (mod Q)
+        al = [(self.__bind(lambda get, **kwargs: get(cl) * util.modinv(get(bl), Q) % Q), 1)]
+        self.gates.append((al, bl, cl))
+        return al
+    def SQRT(self, al): # return a ** (1 / 2) (mod Q)
+        xl = [(self.__bind(lambda get, **kwargs: util.adleman(get(al), Q)), 1)]
+        self.gates.append((xl, xl, al))
+        return xl
+    def POW(self, el, al, N): # return a ** N (mod Q)
         if N < 0:
             N = -N
-            a = self.DIV(e, a)
+            al = self.DIV(el, al)
         if N & 1:
-            e = a
+            el = al
         N >>= 1
         while N:
-            a = self.MUL(a, a)
+            al = self.MUL(al, al)
             if N & 1:
-                e = self.MUL(e, a)
+                el = self.MUL(el, al)
             N >>= 1
-        return e
-    def BOOL(self, xs): # return if xs == 0 then 0 else 1
-        i = self.__bind(lambda witness, **kwargs: pow(sum(witness[x] * m for x, m in xs), Q - 2, Q))
-        o = self.__bind(lambda witness, **kwargs: pow(sum(witness[x] * m for x, m in xs), Q - 1, Q))
-        self.gates.append((xs, [(o, 1)], xs))
-        self.gates.append((xs, [(i, 1)], [(o, 1)]))
-        return o
-    def BIN(self, e, xs, L): # return binary representation of xs (L bits) (experiemental)
-        bs = []
-        closure = lambda I: lambda witness, **kwargs: sum(witness[x] * m for x, m in xs) % Q >> I & 1
+        return el
+    def BOOL(self, xl): # return if x == 0 then 0 else 1
+        il = [(self.__bind(lambda get, **kwargs: pow(get(xl), Q - 2, Q)), 1)]
+        ol = [(self.__bind(lambda get, **kwargs: pow(get(xl), Q - 1, Q)), 1)]
+        self.gates.append((ol, xl, xl))
+        self.gates.append((xl, il, ol))
+        return ol
+    def BIN(self, el, xl, L): # return binary representation of x (L bits) (experiemental)
+        bl = []
+        closure = lambda I: lambda get, **kwargs: get(xl) >> I & 1
         for I in range(L):
-            i = self.__bind(closure(I))
-            self.gates.append(([(i, 1)], [(i, 1)], [(i, 1)]))
-            bs.append((i, 1 << I))
-        self.asserts.append(lambda witness, **kwargs: sum(witness[x] * m for x, m in xs) % Q == sum(witness[i] * m for i, m in bs) * witness[e] % Q)
-        self.gates.append((bs, [(e, 1)], xs))
-        return bs
-    def DEC(self, xs, LIST): # return decoding of xs (LIST is a list of possible values) (experiemental)
-        ds = []
-        es = []
-        closure = lambda V: lambda witness, **kwargs: sum(witness[x] * m for x, m in xs) % Q == V
+            iv = self.__bind(closure(I))
+            il = [(iv, 1)]
+            self.gates.append((il, il, il))
+            bl.append((iv, 1 << I))
+        self.asserts.append(lambda get, **kwargs: get(xl) == get(bl))
+        self.gates.append((el, xl, bl))
+        return bl
+    def DEC(self, el, xl, LIST): # return decoding of x (LIST is a list of possible values) (experiemental)
+        dl = []
+        fl = []
+        closure = lambda V: lambda get, **kwargs: int(get(xl) == V)
         for V in LIST:
-            i = self.__bind(closure(V))
-            self.gates.append(([(i, 1)], [(i, 1)], [(i, 1)]))
-            ds.append((i, V))
-            es.append((i, 1))
-        self.asserts.append(lambda witness, **kwargs: sum(witness[x] * m for x, m in xs) % Q == sum(witness[i] * m for i, m in ds) * witness[e] % Q)
-        self.asserts.append(lambda witness, **kwargs: sum(witness[i] * m for i, m in es) * sum(witness[i] * m for i, m in es) % Q == witness[e] % Q)
-        self.gates.append((ds, [(e, 1)], xs))
-        self.gates.append((es, es, [(e, 1)]))
-        return ds
-    def AND(self, e, a, b, a_flip = False, b_flip = False): # return (a ^ a_flip) & (b ^ b_flip)
-        xs = [(a, -1), (e, 1)] if a_flip else [(a, 1)]
-        ys = [(b, -1), (e, 1)] if b_flip else [(b, 1)]
-        zs = [(0, 1)]
-        (_, N), *zs = zs
-        return self.VMUL(xs, ys, zs, N)
-    def OR(self, e, a, b, a_flip = False, b_flip = False): # return (a ^ a_flip) | (b ^ b_flip)
-        xs = [(a, 1)] if a_flip else [(a, -1), (e, 1)]
-        ys = [(b, 1)] if b_flip else [(b, -1), (e, 1)]
-        zs = [(0, -1), (e, 1)]
-        (_, N), *zs = zs
-        return self.VMUL(xs, ys, zs, N)
-    def XOR(self, e, a, b, r_flip = False): # return a ^ b ^ r_flip
-        xs = [(a, 2), (e, -1)]
-        ys = [(b, 2), (e, -1)]
-        zs = [(0, 2), (e, -1)] if r_flip else [(0, -2), (e, 1)]
-        (_, N), *zs = zs
-        return self.VMUL(xs, ys, zs, N)
-    def COND(self, c, ts, fs): # return if c then ts else fs (c should be 0 or 1)
-        ts = [(t, +n) for t, n in ts]
-        fs = [(f, -n) for f, n in fs]
-        return self.VMUL([(c, 1)], ts + fs, fs)
-    def ASSERT_BOOL(self, x): # assert x == 0 or x == 1
-        self.asserts.append(lambda witness, **kwargs: witness[x] * witness[x] % Q == witness[x] % Q)
-        self.gates.append(([(x, 1)], [(x, 1)], [(x, 1)]))
-    def ASSERT_ZERO(self, e, xs): # assert xs == 0 (mod Q)
-        self.asserts.append(lambda witness, **kwargs: witness[e] * witness[e] % Q == (sum(witness[x] * m for x, m in xs) + witness[e]) % Q)
-        self.gates.append(([(e, 1)], [(e, 1)], [(e, 1)] + xs))
-    def ASSERT_NONZ(self, e, xs): # assert xs != 0 (mod Q)
-        i = self.__bind(lambda witness, **kwargs: pow(sum(witness[x] * m for x, m in xs), Q - 2, Q) * witness[e] % Q)
-        self.asserts.append(lambda witness, **kwargs: sum(witness[x] * m for x, m in xs) * witness[i] % Q == witness[e] % Q)
-        self.gates.append((xs, [(i, 1)], [(e, 1)]))
+            iv = self.__bind(closure(V))
+            il = [(iv, 1)]
+            self.gates.append((il, il, il))
+            dl.append((iv, V))
+            fl.append((iv, 1))
+        self.asserts.append(lambda get, **kwargs: get(xl) == get(dl))
+        self.asserts.append(lambda get, **kwargs: get(fl) == get(el))
+        self.gates.append((el, xl, dl))
+        self.gates.append((el, el, fl))
+        return dl
+    def AND(self, el, al, bl): # return a & b
+        return self.MUL(al, bl)
+    def OR(self, el, al, bl): # return a | b
+        return self.SUB(el, self.MUL(self.SUB(el, al), self.SUB(el, bl)))
+    def XOR(self, el, al, bl): # return a ^ b
+        return self.DIVN(self.SUB(el, self.MUL(self.SUB(el, self.MULN(al, 2)), self.SUB(el, self.MULN(bl, 2)))), 2)
+    def COND(self, cl, tl, fl): # return if c then t else f (c should be 0 or 1)
+        return self.MUL(cl, self.SUB(tl, fl)) + fl
+    def ASSERT(self, xl, yl, zl): # assert x * y == z (mod Q)
+        self.asserts.append(lambda get, **kwargs: get(xl) * get(yl) % Q == get(zl))
+        self.gates.append((xl, yl, zl))
+    def ASSERT_BOOL(self, xl): # assert x == 0 or x == 1
+        self.ASSERT(xl, xl, xl)
+    def ASSERT_ZERO(self, xl): # assert x == 0 (mod Q)
+        self.ASSERT([], [], xl)
+    def ASSERT_NONZ(self, el, xl): # assert x != 0 (mod Q)
+        self.DIV(el, xl)
 def prod(s):
     # generate polynomial t(x) = prod(x - k) for k in s
     t = [1]
@@ -145,35 +157,61 @@ if __name__ == '__main__':
     print('GF({})'.format(Q))
     # Example Program
     pro = Program()
-    e = pro.VAR('e') # 1
-    x = pro.VAR('x') # x
-    y = pro.VAR('y') # y
-    xs = pro.BIN(e, [(x, 1)], 4) # xs = binary(x, 4)
-    ys = pro.BIN(e, [(y, 1)], 4) # ys = binary(y, 4)
-    zs = [(pro.XOR(e, a, b), 1 << i) for i, ((a, _), (b, _)) in enumerate(zip(xs, ys))] # zs = xs ^ ys
-    ILST = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF]
-    OLST = [0x7, 0xC, 0xB, 0xD, 0xE, 0x4, 0x9, 0xF, 0x6, 0x3, 0x8, 0xA, 0x2, 0x5, 0x1, 0x0]
-    os = [(k, o) for (k, i), o in zip(pro.DEC(zs, ILST), OLST)] # os = map(zs, ILST -> OLST)
-    o = pro.VMUL(os, [(e, 1)]) # w = sum(ws)
+    el = pro.VAR('e', reveal = 1) # 1
+    il = pro.VAR('i', reveal = 0) # x
+    OP = [
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+        0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+        0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
+    ]
+    FP = [
+        0x39, 0x31, 0x29, 0x21, 0x19, 0x11, 0x09, 0x01, 0x3b, 0x33, 0x2b, 0x23, 0x1b, 0x13, 0x0b, 0x03,
+        0x3d, 0x35, 0x2d, 0x25, 0x1d, 0x15, 0x0d, 0x05, 0x3f, 0x37, 0x2f, 0x27, 0x1f, 0x17, 0x0f, 0x07,
+        0x38, 0x30, 0x28, 0x20, 0x18, 0x10, 0x08, 0x00, 0x3a, 0x32, 0x2a, 0x22, 0x1a, 0x12, 0x0a, 0x02,
+        0x3c, 0x34, 0x2c, 0x24, 0x1c, 0x14, 0x0c, 0x04, 0x3e, 0x36, 0x2e, 0x26, 0x1e, 0x16, 0x0e, 0x06,
+    ]
+    GP = [
+        0x27, 0x07, 0x2f, 0x0f, 0x37, 0x17, 0x3f, 0x1f, 0x26, 0x06, 0x2e, 0x0e, 0x36, 0x16, 0x3e, 0x1e,
+        0x25, 0x05, 0x2d, 0x0d, 0x35, 0x15, 0x3d, 0x1d, 0x24, 0x04, 0x2c, 0x0c, 0x34, 0x14, 0x3c, 0x1c,
+        0x23, 0x03, 0x2b, 0x0b, 0x33, 0x13, 0x3b, 0x1b, 0x22, 0x02, 0x2a, 0x0a, 0x32, 0x12, 0x3a, 0x1a,
+        0x21, 0x01, 0x29, 0x09, 0x31, 0x11, 0x39, 0x19, 0x20, 0x00, 0x28, 0x08, 0x30, 0x10, 0x38, 0x18,
+    ]
+    fl = [(k, o) for (k, i), o in zip(pro.DEC(el, il, OP), FP)] # fl = map(il, OP -> FP)
+    gl = [(k, o) for (k, i), o in zip(pro.DEC(el, il, OP), GP)] # gl = map(il, OP -> GP)
+    fb = pro.BIN(el, fl, 6) # fb = binary(fs, 6)
+    gb = pro.BIN(el, gl, 6) # gb = binary(gs, 6)
+    ob = [pair for (a, i), (b, i) in zip(fb, gb) for pair in pro.MULN(pro.XOR(el, [(a, 1)], [(b, 1)]), i)] # ob = fb ^ gb
+    ol = pro.RET(ob) # ol = reveal(ob)
     # Compile
-    A, B, C = pro.R1CS() # A, B, C matrices
-    s = util.sample(1, Q, pro.gate_count())
-    t = prod(s)
-    A = convert(A, s) # A polynomials set
-    B = convert(B, s) # B polynomials set
-    C = convert(C, s) # C polynomials set
-    print('t(x) =', util.polyshow(t))
+    print('Gates:', pro.gate_count())
+    print('Vars:', pro.var_count())
+    with Timer('Generating R1CS...'):
+        A, B, C = pro.R1CS() # A, B, C matrices
+    with Timer('Converting to QAP...'):
+        s = util.sample(1, Q, pro.gate_count())
+        t = prod(s)
+        A = convert(A, s) # A polynomials set
+        B = convert(B, s) # B polynomials set
+        C = convert(C, s) # C polynomials set
     # Prove
-    w = pro.witness(e = 1, x = 3, y = 5)
+    with Timer('Calculating witness...'):
+        w = pro.witness(e = 1, i = 48)
     print('witness =', w)
-    a = dot(A, w)
-    b = dot(B, w)
-    c = dot(C, w)
-    print('a(x) =', util.polyshow(a))
-    print('b(x) =', util.polyshow(b))
-    print('c(x) =', util.polyshow(c))
-    d = util.polysub(util.polymul(a, b, Q), c, Q)
-    print('d(x) =', util.polyshow(d))
-    q, r = util.polydm(d, t, Q)
-    print('q(x) =', util.polyshow(q))
-    print('r(x) =', util.polyshow(r))
+    with Timer('Verifying witness...'):
+        a = dot(A, w)
+        b = dot(B, w)
+        c = dot(C, w)
+        d = util.polysub(util.polymul(a, b, Q), c, Q)
+        q, r = util.polydm(d, t, Q)
+    if any(r):
+        print('Verification failed!')
+        print('t(x) =', util.polyshow(t))
+        print('a(x) =', util.polyshow(a))
+        print('b(x) =', util.polyshow(b))
+        print('c(x) =', util.polyshow(c))
+        print('d(x) =', util.polyshow(d))
+        print('q(x) =', util.polyshow(q))
+        print('r(x) =', util.polyshow(r))
+    else:
+        print('Verification passed!')
