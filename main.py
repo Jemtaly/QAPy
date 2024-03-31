9df806ccb7632789861559cc9717608ef57a7249
Jemtaly <Jemtaly@outlook.com>
2024-04-01 01:15:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     parser_prove.add_argument('-g', '--gates', type = str, default = 'a.gates', help = 'The file to read the constraints from. (Default: a.gates)')
     parser_prove.add_argument('-f', '--funcs', type = str, default = 'a.funcs', help = 'The file to read the witness generation functions from. (Default: a.funcs)')
     parser_prove.add_argument('-p', '--prove', type = str, default = 'a.prove', help = 'The file to read the parameters for proving from. (Default: a.prove)')
-    parser_prove.add_argument('-a', '--args', action = StoreDictKeyPair, nargs = '*', default = {}, help = 'The public witness.')
+    parser_prove.add_argument('-a', '--args', action = StoreDictKeyPair, nargs = '*', default = {}, help = 'The arguments to the program, in the form of key=value pairs.')
     parser_prove.add_argument('-P', '--proof', type = str, default = 'a.proof', help = 'The file to write the proof to. (Default: a.proof)')
     parser_verif = subparsers.add_parser('verify', help = 'Verify a proof.', description = 'Verify a proof.')
     parser_verif.add_argument('-n', '--names', type = str, default = 'a.names', help = 'The file to read the public variable names from. (Default: a.names)')
@@ -41,24 +41,30 @@ def main():
     if args.command == 'compile':
         test = compiler.Compiler()
         with open(args.program, 'r') as program:
+            print('Compiling program...')
             test.compile(program.read())
         print('Length of the witness vector:', test.wire_count)
         print('Number of constraints:', len(test.gates))
         print('Number of public variables:', len(test.stmts))
         with open(args.gates, 'wb') as gates:
+            print('Writing constraints to', args.gates)
             gates.write(dill.dumps((test.wire_count, test.stmts.keys(), test.gates)))
         with open(args.funcs, 'wb') as funcs:
+            print('Writing witness generation functions to', args.funcs)
             funcs.write(dill.dumps(test.funcs))
         with open(args.names, 'wb') as names:
+            print('Writing public variable names to', args.names)
             names.write(dill.dumps(test.stmts.values()))
-        # A malicious attacker can tamper with the above files, allowing them to execute arbitrary code when the user
-        # recovers the data by calling dill.loads, so it is recommended that the party executing the compilation sign
-        # these files before distributing them.
+        # A malicious attacker can tamper with the above files, allowing them to execute arbitrary code when the user loads
+        # the files, so it is recommended that the party executing the compilation sign the files before distributing them.
     elif args.command == 'setup':
         with open(args.gates, 'rb') as gates:
+            print('Loading constraints from', args.gates)
             wire_count, skeys, gates = dill.loads(gates.read())
+        print('Setting up parameters for proving and verifying...')
         α1, β1, δ1, β2, γ2, δ2, u1U, v1V, x1I, x2I, y1I = groth16.setup(wire_count, skeys, gates)
         with open(args.prove, 'wb') as prove:
+            print('Writing parameters for proving to', args.prove)
             prove.write(α1.serialize())
             prove.write(β1.serialize())
             prove.write(δ1.serialize())
@@ -69,6 +75,7 @@ def main():
             for x2 in x2I: prove.write(x2.serialize())
             for y1 in y1I: prove.write(y1.serialize())
         with open(args.verif, 'wb') as verif:
+            print('Writing parameters for verifying to', args.verif)
             verif.write(α1.serialize())
             verif.write(β2.serialize())
             verif.write(γ2.serialize())
@@ -76,8 +83,10 @@ def main():
             for u1 in u1U: verif.write(u1.serialize())
     elif args.command == 'prove':
         with open(args.gates, 'rb') as gates:
+            print('Loading constraints from', args.gates)
             wire_count, skeys, gates = dill.loads(gates.read())
         with open(args.funcs, 'rb') as funcs:
+            print('Loading witness generation functions from', args.funcs)
             funcs = dill.loads(funcs.read())
         N = len(gates)
         M = wire_count
@@ -85,6 +94,7 @@ def main():
         V = M - U
         I = 1 << (N - 1).bit_length()
         with open(args.prove, 'rb') as prove:
+            print('Loading parameters for proving from', args.prove)
             α1 = pymcl.G1.deserialize(prove.read(L1))
             β1 = pymcl.G1.deserialize(prove.read(L1))
             δ1 = pymcl.G1.deserialize(prove.read(L1))
@@ -94,27 +104,33 @@ def main():
             x1I = [pymcl.G1.deserialize(prove.read(L1)) for _ in range(I)]
             x2I = [pymcl.G2.deserialize(prove.read(L2)) for _ in range(I)]
             y1I = [pymcl.G1.deserialize(prove.read(L1)) for _ in range(I)]
+        print('Generating proof...')
         A1, B2, C1, uU = groth16.prove(wire_count, funcs, skeys, gates, α1, β1, δ1, β2, δ2, v1V, x1I, x2I, y1I, args.args)
         with open(args.proof, 'wb') as proof:
+            print('Writing proof to', args.proof)
             proof.write(A1.serialize())
             proof.write(B2.serialize())
             proof.write(C1.serialize())
             for u in uU: proof.write(u.to_bytes(L0, 'big'))
     elif args.command == 'verify':
         with open(args.names, 'rb') as names:
+            print('Loading public variable names from', args.names)
             names = dill.loads(names.read())
         U = len(names)
         with open(args.verif, 'rb') as verif:
+            print('Loading parameters for verifying from', args.verif)
             α1 = pymcl.G1.deserialize(verif.read(L1))
             β2 = pymcl.G2.deserialize(verif.read(L2))
             γ2 = pymcl.G2.deserialize(verif.read(L2))
             δ2 = pymcl.G2.deserialize(verif.read(L2))
             u1U = [pymcl.G1.deserialize(verif.read(L1)) for _ in range(U)]
         with open(args.proof, 'rb') as proof:
+            print('Loading proof from', args.proof)
             A1 = pymcl.G1.deserialize(proof.read(L1))
             B2 = pymcl.G2.deserialize(proof.read(L2))
             C1 = pymcl.G1.deserialize(proof.read(L1))
             uU = [int.from_bytes(proof.read(L0), 'big') for _ in range(U)]
+        print('Verifying proof...')
         passed, outs = groth16.verify(names, α1, β2, γ2, δ2, u1U, A1, B2, C1, uU)
         if passed:
             print('Verification passed!')
